62f62967deb5252bfd9ae4fd85534a59ac77d3f7
Bram Vandenbussche <bram@vandenbussche.me>
2022-07-30 00:56:45 +0200

    Store data in table storage

--- bramvandenbussche.readwiser.api/Controllers/HighlightController.cs
+++ bramvandenbussche.readwiser.api/Controllers/HighlightController.cs
@@ -1,4 +1,6 @@
 using System.Collections.Concurrent;
+using bramvandenbussche.readwiser.api.DataAccess;
+using bramvandenbussche.readwiser.api.Domain;
 using bramvandenbussche.readwiser.api.Dto;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,12 +11,14 @@ namespace bramvandenbussche.readwiser.api.Controllers
     public class HighlightController : ControllerBase
     {
         private static ConcurrentBag<HighlightDto> _cache = new();
+        private readonly INoteRepository _repository;
 
         private readonly ILogger<HighlightController> _logger;
 
-        public HighlightController(ILogger<HighlightController> logger)
+        public HighlightController(ILogger<HighlightController> logger, INoteRepository repository)
         {
             _logger = logger;
+            _repository = repository;
         }
 
         [HttpGet]
@@ -26,9 +30,10 @@ namespace bramvandenbussche.readwiser.api.Controllers
         [HttpPost()]
         public ActionResult AddNewHighlight([FromBody] HighlightRequestDto request)
         {
-            foreach (var h in request.Highlights)
+            foreach (var note in request.Highlights)
             {
-                _cache.Add(h);
+                _cache.Add(note);
+                _repository.Save(note.ToDomain());
             }
 
             return Ok(_cache.Count);
---
+++ bramvandenbussche.readwiser.api/Dto/DtoExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using bramvandenbussche.readwiser.api.Domain;
+
+namespace bramvandenbussche.readwiser.api.Dto;
+
+public static class DtoExtensions
+{
+    public static Highlight ToDomain(this HighlightDto dto)
+    {
+        return new Highlight()
+        {
+            NoteId = Guid.NewGuid(),
+            RaisedTime = DateTimeOffset.UtcNow,
+            Author = dto.Author,
+            Title = dto.Title,
+            Chapter = dto.Chapter,
+            Text = dto.Text
+        };
+    }
+
+}
