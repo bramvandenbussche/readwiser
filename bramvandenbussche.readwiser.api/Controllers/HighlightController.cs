45b9e81267fb78385f05a1e55ab1d49e62da1132
Bram Vandenbussche <bram@vandenbussche.me>
2023-09-19 22:04:21 +0200

    Improve logging

--- bramvandenbussche.readwiser.api/Controllers/HighlightController.cs
+++ bramvandenbussche.readwiser.api/Controllers/HighlightController.cs
@@ -12,21 +12,25 @@ namespace bramvandenbussche.readwiser.api.Controllers
     public class HighlightController : ControllerBase
     {
         private readonly IHighlightService _service;
+        private readonly INoteMigrationService _migrationService;
 
         private readonly ILogger<HighlightController> _logger;
 
-        public HighlightController(ILogger<HighlightController> logger, IHighlightService service)
+        public HighlightController(IHighlightService service, INoteMigrationService migrationService, ILogger<HighlightController> logger)
         {
-            _logger = logger;
             _service = service;
+            _logger = logger;
+            _migrationService = migrationService;
         }
 
         [HttpGet]
         [Authorize]
         public async Task<ActionResult> GetAll(int timestamp = 0)
         {
+            _logger.LogDebug($"{nameof(GetAll)}: Request received with timestamp: {timestamp}");
             var data = await _service.GetAll(timestamp);
 
+            _logger.LogInformation($"{nameof(GetAll)}: Returned {data.Count()} notes");
             return Ok(data.ToDto());
         }
 
@@ -34,8 +38,10 @@ namespace bramvandenbussche.readwiser.api.Controllers
         [Authorize]
         public async Task<ActionResult> GetHighlightsForBook(string title, string author)
         {
+            _logger.LogDebug($"{nameof(GetHighlightsForBook)}: Request received for book: {author} - {title}");
             var data = await _service.GetForBook(title, author);
 
+            _logger.LogInformation($"{nameof(GetHighlightsForBook)}: Returned {data.Count()} notes");
             return Ok(data.ToDto());
         }
 
@@ -43,9 +49,11 @@ namespace bramvandenbussche.readwiser.api.Controllers
         [Authorize]
         public async Task<ActionResult> AddNewHighlight([FromBody] CreateHighlightRequestDto request)
         {
+            _logger.LogDebug($"{nameof(AddNewHighlight)}: Request received containing {request.Highlights.Count} notes. First book: {request.Highlights[0].Author} - {request.Highlights[0].Title}");
             try
             {
                 await _service.Add(request.Highlights);
+                _logger.LogInformation($"{nameof(AddNewHighlight)}: Saved {request.Highlights.Count} notes");
             }
             catch (Exception e)
             {
@@ -59,10 +67,20 @@ namespace bramvandenbussche.readwiser.api.Controllers
 
         [HttpPost("migrate")]
         [Authorize]
-        public async Task<ActionResult> MigrateStorage([FromBody] string source)
+        public async Task<ActionResult> MigrateStorage([FromBody] MigrateNotesRequest source)
         {
+            _logger.LogDebug($"{nameof(MigrateStorage)}: Request received with timestamp: {source.Timestamp}");
+            var result = await _migrationService.ImportAll(source.SourceConnection, source.Timestamp);
 
-            return Ok();
+            if (result.IsSucces)
+            {
+                _logger.LogInformation(
+                    $"{nameof(MigrateStorage)}: Migration finished. Imported {result.Notes} notes for {result.Books} books.");
+                return Ok(result);
+            }
+
+            _logger.LogError($"{nameof(MigrateStorage)}: Migration failed. Reason: {result.Reason}.");
+            return Problem(result.Reason);
         }
     }
 }
