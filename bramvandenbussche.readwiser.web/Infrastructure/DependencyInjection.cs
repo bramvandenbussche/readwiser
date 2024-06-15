7bf5fdc803122ac8254e2740475f079a6896afdc
Bram Vandenbussche <bram@vandenbussche.me>
2024-06-15 16:53:06 +0200

    Add Blazor frontend

---
+++ bramvandenbussche.readwiser.web/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,40 @@
+﻿using bramvandenbussche.readwiser.data.mongodb;
+using bramvandenbussche.readwiser.domain.Model;
+using MongoDB.Thin;
+
+namespace bramvandenbussche.readwiser.web.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Load executable / API
+        services.Scan(scan => scan
+             .FromAssemblyOf<Program>()
+             .AddClasses()
+             .AsSelf()
+             .AsImplementedInterfaces()
+             .WithSingletonLifetime());
+
+        // Load Domain
+        services.Scan(scan => scan
+            .FromAssemblyOf<Highlight>()
+            .AddClasses()
+            .AsSelf()
+            .AsImplementedInterfaces()
+            .WithSingletonLifetime());
+
+        // Load DataAccess
+        //  - MongoDb
+        services.AddMongo(configuration["DataStore:MongoDb:ConnectionString"],
+            configuration["DataStore:MongoDb:Database"]);
+        services.Scan(scan => scan
+            .FromAssemblyOf<MongoDbNoteRepository>()
+            .AddClasses()
+            .AsSelf()
+            .AsImplementedInterfaces()
+            .WithSingletonLifetime());
+
+        return services;
+    }
+}
---
+++ bramvandenbussche.readwiser.web/Program.cs
@@ -0,0 +1,26 @@
+using bramvandenbussche.readwiser.web.Components;
+using bramvandenbussche.readwiser.web.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services
+    .AddDependencyInjection(builder.Configuration)
+    .AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
